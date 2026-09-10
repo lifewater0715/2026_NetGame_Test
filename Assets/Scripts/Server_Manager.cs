09a5a1b61ee2eb38fb05a5785cbced7c4892257a
KYS <dydte1206@naver.com>
2026-09-10 18:11:14 +0900

    프로잭트 시작

---
+++ Assets/Scripts/Server_Manager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+using System;
+using TMPro;
+using System.Linq;
+
+public class Server_Manager : MonoBehaviour
+{
+    [SerializeField] private NetworkManager networkManager;
+    [SerializeField] private TMP_Text serverStateUI;
+    [SerializeField] private char serverState = 'N';
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        Server_S_UI('N');
+        networkManager = gameObject.GetComponent<NetworkManager>();
+    }
+
+    public void HostStart()
+    {
+        Server_S_UI('H');
+        networkManager.StartHost();
+    }
+    public void ServerStart()
+    {
+        Server_S_UI('S');
+        networkManager.StartServer();
+    }
+    public void ClientStart()
+    {
+        Server_S_UI('C');
+        networkManager.StartClient();
+    }
+
+    private void Server_S_UI(char serverStateText)
+    {
+        string Text = "Current State : <" + serverStateText + ">";
+        serverStateUI.text = Text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
